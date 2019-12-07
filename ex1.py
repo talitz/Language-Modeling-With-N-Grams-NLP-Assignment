a12b1e5e99443119ebdba6408ec4580e8d194e32
tyitzhak <tal_yitzhak@intuit.com>
2019-12-07 17:30:04 +0200

    fixed math error

--- ex1.py
+++ ex1.py
@@ -146,78 +146,78 @@ class Ngram_Language_Model:
            Returns:
                Float. The float should reflect the (log) probability.
         """
-        print("Evaluating: {}".format(text))
+        #print("Evaluating: {}".format(text))
         sum_likelihood = 0.0
         split_text = text.split()
         for index in range(len(split_text)):
             count_seq = 0
             count_prefix = 0
-            print("index = {}, split_text = {}".format(index,split_text))
+            #print("index = {}, split_text = {}".format(index,split_text))
             given_text = normalize_text(text)
-            print("normalize text = {}".format(given_text))
+            #print("normalize text = {}".format(given_text))
             if index - (self.n - 1) < 0:
-                print("flow 1")
+                #print("flow 1")
                 prefix = ' '.join([x for x in split_text[0:index]])
                 suffix = split_text[index]
-                print("prefix = {}".format(prefix))
-                print("suffix = {}".format(suffix))
+                #print("prefix = {}".format(prefix))
+                #print("suffix = {}".format(suffix))
                 if not self.chars:
                     seq = prefix + ' ' + suffix
                 else:
                     seq = prefix + suffix
                 if index == 0:
-                    print("flow 1->2")
+                    #print("flow 1->2")
                     seq = seq.replace(' ', '')
-                    print("seq = {}".format(seq))
+                    #print("seq = {}".format(seq))
                     for key, value in self.ngrams_dictionaries[self.n].items():
                         if key.startswith(seq):
                             count_seq = count_seq + value
                     count_prefix = count_prefix + len(self.text.split()) - self.n + 1
                 else:
-                    print("flow 1->3")
-                    print("seq = {}".format(seq))
-                    print("count_seq = {}".format(count_seq))
+                    #print("flow 1->3")
+                    #print("seq = {}".format(seq))
+                    #print("count_seq = {}".format(count_seq))
                     for key, value in self.ngrams_dictionaries[self.n].items():
                             if key.startswith(seq):
                                 count_seq = count_seq + value
-                                print("count_seq = {}".format(count_seq))
-                                print("key = {} STARTS!!! with = {}".format(key,seq))
-                            else:
-                                print("key = {} doesn't start with = {}".format(key,seq))
+                                #print("count_seq = {}".format(count_seq))
+                                #print("key = {} STARTS!!! with = {}".format(key,seq))
+                            #else:
+                                #print("key = {} doesn't start with = {}".format(key,seq))
                     count_prefix = self._count_check_anagram(prefix)
-                print("count_prefix = {}".format(count_prefix))
-                print("count_seq = {}".format(count_seq))
-                #if seq not in self.ngrams_dictionaries[self.n].keys():
-                #    print("seq = {}, smoothing!".format(seq))
-                #    return math.log(self.smooth(seq))
+                    #print("count_prefix = {}".format(count_prefix))
+                    #print("count_seq = {}".format(count_seq))
+                    if count_seq == 0:
+                        #print("seq = {}, smoothing!".format(seq))
+                        return math.log(self.smooth(seq))
             else:
-                print("flow 2")
+                #print("flow 2")
                 prefix = split_text[(index - self.n + 1): index]
                 suffix = split_text[index]
-                print("prefix = {}".format(prefix))
-                print("suffix = {}".format(suffix))
+                #print("prefix = {}".format(prefix))
+                #print("suffix = {}".format(suffix))
                 if self.chars:
                     seq = (''.join([x for x in prefix])) + suffix
                 else:
                     seq = (' '.join([x for x in prefix])) + ' ' + suffix
                 if seq not in self.ngrams_dictionaries[self.n].keys():
-                    print("seq = {}, smoothing!".format(seq))
+                    #print("seq = {}, smoothing!".format(seq))
                     return math.log(self.smooth(seq))
                 count_seq = 0
                 for key, value in self.ngrams_dictionaries[self.n].items():
                         if key.startswith(seq):
                             count_seq = count_seq + value
-                        else:
-                            print("key = {} doesn't start with = {}".format(key,seq))
+                        #else:
+                            #print("key = {} doesn't start with = {}".format(key,seq))
                 count_prefix = 0
-                for key, value in self.ngrams_dictionaries[self.n].items():
+                for key, value in self.ngrams_dictionaries[self.n].items() or count_seq == 0:
                         if key.startswith(' '.join(prefix)):
                             count_prefix = count_prefix + value
-                print("count_prefix = {}".format(count_prefix))
-                print("count_seq = {}".format(count_seq))
+                #print("count_prefix = {}".format(count_prefix))
+                #print("count_seq = {}".format(count_seq))
             sum_likelihood += math.log((count_seq / count_prefix))   
-            print("------------")
-        print("sum of likelihood = {}".format(sum_likelihood))
+            #print("------------")
+        #print("sum of likelihood = {}".format(sum_likelihood))
         return sum_likelihood
 
     def _count_check_anagram(self, prefix):
